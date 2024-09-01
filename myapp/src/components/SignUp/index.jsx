a6be248864c9a8bfe5853ae0d34635600d21ad94
Nada-ibrahim12 <123187920+Nada-ibrahim12@users.noreply.github.com>
2024-09-01 21:32:01 +0300

    getData for registeration form

--- myapp/src/components/SignUp/index.jsx
+++ myapp/src/components/SignUp/index.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "../Navbar/index";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "@fortawesome/fontawesome-free/css/all.min.css"; 
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import { Outlet, Link } from "react-router-dom";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  function getData(e) {
+    let data = { ...formData };
+    data[e.target.name] = e.target.value;
+    setFormData(data);
+  }
+  function submitHandler() {}
   return (
     <div>
       <NavBar />
@@ -17,20 +28,19 @@ export default function SignUp() {
                 <div className="card-body">
                   <div className="row justify-content-center">
                     <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-                      <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-2">
+                      <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-5">
                         Sign up
                       </p>
-                      <form className="mx-1 mx-md-4">
+                      <form onSubmit={submitHandler} className="mx-1 mx-md-4">
                         <div className="d-flex flex-row align-items-center mb-4">
                           <i className="fas fa-user fa-lg me-3 fa-fw"></i>
                           <div className="form-outline flex-fill mb-0">
-                            <label
-                              className="form-label"
-                              htmlFor="form3Example1c"
-                            >
+                            <label className="form-label" htmlFor="">
                               Your Name
                             </label>
                             <input
+                              onChange={getData}
+                              name="name"
                               type="text"
                               id="form3Example1c"
                               className="form-control"
@@ -40,13 +50,12 @@ export default function SignUp() {
                         <div className="d-flex flex-row align-items-center mb-4">
                           <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
                           <div className="form-outline flex-fill mb-0">
-                            <label
-                              className="form-label"
-                              htmlFor="form3Example3c"
-                            >
+                            <label className="form-label" htmlFor="">
                               Your Email
                             </label>
                             <input
+                              // onChange={}
+                              name="email"
                               type="email"
                               id="form3Example3c"
                               className="form-control"
@@ -56,29 +65,12 @@ export default function SignUp() {
                         <div className="d-flex flex-row align-items-center mb-4">
                           <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
                           <div className="form-outline flex-fill mb-0">
-                            <label
-                              className="form-label"
-                              htmlFor="form3Example4c"
-                            >
+                            <label className="form-label" htmlFor="">
                               Password
                             </label>
                             <input
-                              type="password"
-                              id="form3Example4c"
-                              className="form-control"
-                            />
-                          </div>
-                        </div>
-                        <div className="d-flex flex-row align-items-center mb-4">
-                          <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
-                          <div className="form-outline flex-fill mb-0">
-                            <label
-                              className="form-label"
-                              htmlFor="form3Example4c"
-                            >
-                              Confirm Password
-                            </label>
-                            <input
+                              // onChange={}
+                              name="password"
                               type="password"
                               id="form3Example4c"
                               className="form-control"
@@ -87,25 +79,24 @@ export default function SignUp() {
                         </div>
                         <div className="form-check d-flex justify-content-center mb-5">
                           <input
+                            // onChange={}
                             className="form-check-input me-2"
                             type="checkbox"
                             value=""
                             id="form2Example3c"
                           />
-                          <label
-                            className="form-check-label"
-                            htmlFor="form2Example3c"
-                          >
+                          <label className="form-check-label" htmlFor="">
                             I agree all statements in{" "}
                             <Link to="#!">Terms of service</Link>
                           </label>
                         </div>
                         <div className="d-flex justify-content-between col-10 text-center text-lg-start mt-4 pt-2">
                           <button
-                            type="button"
+                            type="submit"
                             data-mdb-button-init
                             data-mdb-ripple-init
                             className="btn btn-dark btn-lg pe-4 ps-4"
+                            onClick={submitHandler}
                           >
                             Register
                           </button>
@@ -122,21 +113,21 @@ export default function SignUp() {
                           </p>
                           <button
                             type="button"
-                            className="btn btn-primary btn-floating mx-1"
+                            className="btn btn-dark btn-floating mx-1"
                           >
                             <i className="fab fa-facebook-f"></i>
                           </button>
 
                           <button
                             type="button"
-                            className="btn btn-primary btn-floating mx-1"
+                            className="btn btn-dark btn-floating mx-1"
                           >
                             <i className="fab fa-twitter"></i>
                           </button>
 
                           <button
                             type="button"
-                            className="btn btn-primary btn-floating mx-1"
+                            className="btn btn-dark btn-floating mx-1"
                           >
                             <i className="fab fa-linkedin-in"></i>
                           </button>
